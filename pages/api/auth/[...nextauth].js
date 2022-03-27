4b39e3eee30ded61ca163acca17f05494b8a79bd
Sean Hopkins <ratchetclnk55@gmail.com>
2022-03-27 09:45:20 -0400

    updated changes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google'
 
 const options = {
-  secret: process.env.SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     // OAuth authentication providers
     GoogleProvider({
