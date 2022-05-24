000d81e3bd3fbc572b64acaaa83ff8a5344d1166
simplicy <ratchetclnk55@gmail.com>
2022-05-23 23:01:12 -0400

    some variable changes

--- pages/accounts/[id]/index.js
+++ pages/accounts/[id]/index.js
@@ -47,7 +47,7 @@ export async function getServerSideProps(params) {
     "Database is not connected!"
   }
   
-  const db = await fetch('http://localhost:3000/api/accounts/', {
+  const db = await fetch(process.env.DB_URL+'/api/accounts/', {
     method: 'GET',
     headers: {
       Accept: 'application/json',
@@ -63,7 +63,7 @@ export async function getServerSideProps(params) {
     return data.email
   }).join(",")
 
-  const api = await fetch('http://localhost:3000/api/calendars/?emails='+userEmails, {
+  const api = await fetch(process.env.DB_URL+'/api/calendars/?emails='+userEmails, {
     method: 'GET',
     headers: {
       Accept: 'application/json',
